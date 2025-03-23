def9dc56fbc9d86892b071f620aa2d5becff752d
Christo Todorov <chroxify@gmail.com>
2025-03-23 15:53:14 +0100

    feat: add seline

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -27,6 +27,11 @@ export default function RootLayout({
           src="https://static.cloudflareinsights.com/beacon.min.js"
           data-cf-beacon='{"token": "79afd4c4712c4091816b45fd29bef4ac"}'
         />
+        <Script
+          async
+          src="https://cdn.seline.so/seline.js"
+          data-token="3cce126529eb953"
+        />
       </head>
       <body className={`${instrumentSans.className} antialiased`}>
         {children}
